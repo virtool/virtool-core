3be20306779ff049062e4cf76635bb832475d9ed
BlakeASmith <blakeinvictoria@gmail.com>
2020-09-22 12:10:12 -0700

    Lost some commit history due to a previous fork being accidentally removed
    Here is a summary of the changes
    
    - ported Collection from virtool/virtool/db/core.py
        - removed `silent` attribute in favor of no-op enqueue_change
        - import in __init__.py for access as db.Collection
    - ported functions from virtool/virtool/db/utils.py as needed
    - copied tests from virtool/virtool/tests/db/test_core.py
    - edited tests in test_core.py
        - inconsistent use of variadic arguments for enqueue_change function
        - edited to always use signature enqueue_change(interface, update, *id_list)
    - copied test fixures from virtool/virtool/tests/fixures

---
+++ tests/fixtures/otus.py
@@ -0,0 +1,114 @@
+import copy
+import gzip
+import json
+import os
+import sys
+
+import pytest
+from aiohttp.test_utils import make_mocked_coro
+
+TEST_FILES_PATH = os.path.join(sys.path[0], "tests", "test_files")
+
+
+@pytest.fixture(scope="session")
+def import_data_file():
+    with gzip.open(os.path.join(TEST_FILES_PATH, "otus.json.gz"), "rt") as f:
+        data = json.load(f)
+
+    return data
+
+
+@pytest.fixture
+def import_data(import_data_file):
+    return copy.deepcopy(import_data_file)
+
+
+@pytest.fixture
+def test_otu():
+    return {
+        "version": 0,
+        "abbreviation": "PVF",
+        "imported": True,
+        "isolates": [
+            {
+                "default": True,
+                "id": "cab8b360",
+                "source_name": "8816-v2",
+                "source_type": "isolate"
+            }
+        ],
+        "last_indexed_version": 0,
+        "lower_name": "prunus virus f",
+        "verified": False,
+        "name": "Prunus virus F",
+        "schema": [],
+        "reference": {
+            "id": "hxn167"
+        },
+        "_id": "6116cba1"
+    }
+
+
+@pytest.fixture
+def test_isolate():
+    return {
+        "id": "cab8b360",
+        "default": True,
+        "source_name": "8816-v2",
+        "source_type": "isolate"
+    }
+
+
+@pytest.fixture
+def test_sequence():
+    return {
+        "_id": "KX269872",
+        "definition": "Prunus virus F isolate 8816-s2 segment RNA2 polyprotein 2 gene, complete cds.",
+        "host": "sweet cherry",
+        "otu_id": "6116cba1",
+        "isolate_id": "cab8b360",
+        "sequence": "TGTTTAAGAGATTAAACAACCGCTTTC",
+        "segment": None
+    }
+
+
+@pytest.fixture
+def test_merged_otu():
+    return {
+        "version": 0,
+        "abbreviation": "PVF",
+        "imported": True,
+        "isolates": [
+            {
+                "default": True,
+                "id": "cab8b360",
+                "sequences": [
+                    {
+                        "_id": "KX269872",
+                        "otu_id": "6116cba1",
+                        "isolate_id": "cab8b360",
+                        "definition": "Prunus virus F isolate 8816-s2 segment RNA2 polyprotein 2 gene, complete cds.",
+                        "host": "sweet cherry",
+                        "sequence": "TGTTTAAGAGATTAAACAACCGCTTTC",
+                        "segment": None
+                    }
+                ],
+                "source_name": "8816-v2",
+                "source_type": "isolate"
+            }
+        ],
+        "reference": {
+            "id": "hxn167"
+        },
+        "last_indexed_version": 0,
+        "lower_name": "prunus virus f",
+        "verified": False,
+        "name": "Prunus virus F",
+        "schema": [],
+        "_id": "6116cba1"
+    }
+
+
+@pytest.fixture(scope="function")
+def import_json(import_json_from_file):
+    return copy.deepcopy(import_json_from_file)
