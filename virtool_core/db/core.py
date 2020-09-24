827d8051cb3b799c84ceb06490c298f37fa9d9b5
BlakeASmith <blake.anthony.smith@gmail.com>
2020-09-23 17:33:14 -0700

    Add `silent` parameter for all operations and update docstrings and add new docstrings for `replace_one`, `replace_many`, `update_one`, `update_many`

--- virtool_core/db/core.py
+++ virtool_core/db/core.py
@@ -88,7 +88,7 @@ class Collection:
         Delete many documents based on the passed `query`.
 
         :param query: a MongoDB query
-        :param silent: don't dispatch websocket messages for this operation
+        :param silent: if True, don't dispatch updates for this operation
         :return: the delete result
 
         """
@@ -101,18 +101,19 @@ class Collection:
 
         return delete_result
 
-    async def delete_one(self, query: dict):
+    async def delete_one(self, query: dict, silent: bool = False):
         """
         Delete a single document based on the passed `query`.
 
         :param query: a MongoDB query
+        :param silent: if True, don't dispatch updates for this operation
         :return: the delete result
 
         """
         document_id = await utils.get_one_field(self, "_id", query)
         delete_result = await self._collection.delete_one(query)
 
-        if delete_result.deleted_count:
+        if delete_result.deleted_count and not silent:
             await self.enqueue_change(
                 "delete",
                 document_id
@@ -125,7 +126,8 @@ class Collection:
             query: dict,
             update: dict,
             projection: Union[None, dict, list] = None,
-            upsert: bool = False
+            upsert: bool = False,
+            silent: bool = False
     ):
         """
         Update a document and return the updated result.
@@ -134,6 +136,8 @@ class Collection:
         :param update: a MongoDB update
         :param projection: a projection to apply to the returned document instead of the default
         :param upsert: insert a new document if the query doesn't match an existing document
+        :param silent: if True, don't dispatch updates for this operation
+
         :return: the updated document
 
         """
@@ -147,17 +151,19 @@ class Collection:
         if document is None:
             return None
 
-        await self.enqueue_change("update", document["_id"])
+        if not silent:
+            await self.enqueue_change("update", document["_id"])
 
         if projection:
             return utils.apply_projection(document, projection)
 
         return document
 
-    async def insert_one(self, document: dict) -> dict:
+    async def insert_one(self, document: dict, silent: bool = False) -> dict:
         """
         Insert a document into the database collection
         :param document: the document to insert
+        :param silent: if True, don't dispatch updates for this operation
         """
 
         if "_id" not in document:
@@ -165,7 +171,8 @@ class Collection:
 
         try:
             await self._collection.insert_one(document)
-            await self.enqueue_change("insert", document["_id"])
+            if not silent:
+                await self.enqueue_change("insert", document["_id"])
 
             return document
         except pymongo.errors.DuplicateKeyError:
@@ -173,7 +180,15 @@ class Collection:
             document.pop("_id")
             return await self._collection.insert_one(document)
 
-    async def replace_one(self, query, replacement, upsert=False):
+    async def replace_one(self, query: dict, replacement: dict, upsert: bool = False, silent: bool = False):
+        """
+        Replace a document in the database collection
+        :param query: the MongoDB query document
+        :param replacement: the new document
+        :param upsert: if True, a new document will be created if none are found.
+        :param silent: if True, updates will not be dispatched
+        :return: the newly added document
+        """
         document = await self._collection.find_one_and_replace(
             query,
             replacement,
@@ -181,26 +196,44 @@ class Collection:
             upsert=upsert
         )
 
-        await self.enqueue_change(
-            "update",
-            replacement["_id"]
-        )
+        if not silent:
+            await self.enqueue_change(
+                "update",
+                replacement["_id"]
+            )
 
         return document
 
-    async def update_many(self, query, update):
+    async def update_many(self, query: dict, update: dict, silent: bool = False):
+        """
+        Apply an update to multiple documents in the collection
+        :param query: The MongoDB query document
+        :param update: The MongoDB update document or replacement document
+        :param silent: if True, updates will not be dispatched
+        :return: The result of the update
+        """
+
         updated_ids = await self.distinct("_id", query)
         update_result = await self._collection.update_many(query, update)
 
-        await self.enqueue_change("update", *updated_ids)
+        if not silent:
+            await self.enqueue_change("update", *updated_ids)
 
         return update_result
 
-    async def update_one(self, query, update, upsert=False):
+    async def update_one(self, query: dict, update: dict, upsert: bool = False, silent: bool = False):
+        """
+        :param query: The MongoDB query document
+        :param update: The MongoDB update document or replacement document
+        :param upsert: If True, the update document will be inserted if there is no document matching the query
+        :param silent: if True, updates will not be dispatched
+        :return: The result of the update
+        """
+
         document = await self.find_one(query, ["_id"])
         update_result = await self._collection.update_one(query, update, upsert=upsert)
 
-        if document:
+        if document and not silent:
             await self.enqueue_change(
                 "update",
                 document["_id"]
