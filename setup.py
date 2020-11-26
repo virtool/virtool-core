af7aaa146375336abc1297eb092a4db3d623e200
Ian Boyes <igboyes@gmail.com>
2020-11-26 14:49:04 -0800

    Update requirements and move to setup.py
    *Remove requirements.txt.
    *Remove unused requirements
    *Update some requirements to avoid warnings about @coroutine

--- setup.py
+++ setup.py
@@ -4,12 +4,10 @@ with open("README.md", "r") as readme:
     long_description = readme.read()
 
 INSTALL_REQUIRES = [
-    "aiofiles==0.4.0",
+    "aiofiles==0.6.0",
     "arrow==0.15.5",
     "dictdiffer==0.8.1",
-    "pymongo>=3.11",
-    "psutil==5.7.0",
-    "motor",
+    "motor==2.3.0"
 ]
 
 setuptools.setup(
