0e41948ff3b03c2029a723a87e98388a03de4c1b
Blake Smith <blakeinvictoria@gmail.com>
2021-07-05 11:20:31 -0700

    Remove redis and mongo connection string options.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,5 @@
 pytest_plugins = [
-    "tests.fixtures.db",
     "tests.fixtures.core",
     "tests.fixtures.history",
     "tests.fixtures.otus",
 ]
-
-
-def pytest_addoption(parser):
-    parser.addoption(
-        "--db-connection-string",
-        action="store",
-        default="mongodb://localhost:27017"
-    )
-
-    parser.addoption(
-        "--redis-connection-string",
-        action="store",
-        default="redis://localhost:6379"
-    )
