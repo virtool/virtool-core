c0cae1c7c1717f009f323646c29beb2efacfa2e6
Blake Smith <blakeinvictoria@gmail.com>
2021-07-05 12:10:10 -0700

    Remove unused test fixtures.

--- docs/conf.py
+++ docs/conf.py
@@ -31,9 +31,8 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "recommonmark",
     "sphinx.ext.autodoc",
-    "sphinx.ext.autoselectionlabel",
+    "sphinx.ext.autosectionlabel",
     "sphinx.ext.intersphinx"
 ]
 
