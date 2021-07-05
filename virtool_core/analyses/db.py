e97d3295086d9a9c5d9c6439c559410f27ab734a
Blake Smith <blakeinvictoria@gmail.com>
2021-07-05 11:31:52 -0700

    Remove analysis and subtractions packages.

--- virtool_core/analyses/db.py
+++
@@ -1,9 +0,0 @@
-from virtool_core.utils import timestamp
-
-async def remove_nuvs_blast(db, analysis_id, sequence_index):
-    await db.analyses.update_one({"_id": analysis_id, "results.index": sequence_index}, {
-        "$set": {
-            "results.$.blast": None,
-            "updated_at": timestamp()
-        }
-    })
