e521fe3450970ffbe500f86ce01e9e949ef362fa
Blake Smith <blakeinvictoria@gmail.com>
2021-07-05 11:28:45 -0700

    Remove otus package.
    This functionality is handled in virtool only and no longer
    belongs in `virtool_core`.

--- tests/otus/test_utils.py
+++
@@ -1,6 +0,0 @@
-import virtool_core.otus.utils
-
-
-def test_merge_otu(test_otu, test_sequence, test_merged_otu):
-    merged = virtool_core.otus.utils.merge_otu(test_otu, [test_sequence])
-    assert merged == test_merged_otu
