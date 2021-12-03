0590f04618122c069b38a3285d6d972dbf257c89
Ian Boyes <igboyes@gmail.com>
2021-12-03 12:07:08 -0800

    feat: add redis connection function
    * Install aioredis.
    * Add a function that returns an aioredis connection pool.
    * Add test.
    * Add Redis service to CI for testing.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,20 @@
 import pytest
 from pathlib import Path
 
+
 @pytest.fixture
 def test_json_path() -> Path:
     return Path(__file__).parent/"models/json"
+
+
+@pytest.fixture
+def redis_connection_string(request):
+    return request.config.getoption("--redis-connection-string")
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--redis-connection-string",
+        action="store",
+        default="redis://localhost:6379",
+    )
