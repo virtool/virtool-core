716d54b16f9915793b1cc8418b873e57e2661e5c
Ryan <ryanfang11@gmail.com>
2022-05-12 15:58:27 -0700

    feat: move logging configuration to core

--- tests/test_redis.py
+++ tests/test_redis.py
@@ -5,4 +5,6 @@ import logging
 async def test_redis_success(caplog, redis_connection_string):
     caplog.set_level(logging.INFO)
     await connect_to_redis(redis_connection_string)
-    assert "Found Redis 6." in caplog.records[0].message
+    for record in caplog.records:
+        if record.levelname == "INFO":
+            assert "Found Redis 6." in record.message
