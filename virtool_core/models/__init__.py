e7c6302563507e6d9693c0c6d14ca4437a444e20
Ian Boyes <igboyes@gmail.com>
2022-06-03 09:56:30 -0700

    feat: add full and minimal user models

--- virtool_core/models/__init__.py
+++ virtool_core/models/__init__.py
@@ -1,6 +1,12 @@
-"""Pydantic models for Virtool records."""
-from .analysis import Analysis
+"""Pydantic models for Virtool resources."""
+from virtool_core.models.analysis import Analysis
+from virtool_core.models.group import Group, Permissions
+from virtool_core.models.user import User, UserMinimal
 
 __all__ = [
     "Analysis",
+    "Group",
+    "Permissions",
+    "User",
+    "UserMinimal",
 ]
