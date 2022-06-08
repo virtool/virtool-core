9414e57c0f4e28a12d724c0035cae6889591ce48
Aman Monga <aman260@outlook.com>
2022-06-08 09:38:20 -0700

    feat: consolidate redis code into core
    BREAKING CHANGE: Changes API for Redis utilities.

--- tests/test_redis.py
+++ tests/test_redis.py
@@ -1,10 +1,10 @@
-from virtool_core.redis import connect_to_redis
+from virtool_core.redis import connect
 import logging
 
 
 async def test_redis_success(caplog, redis_connection_string):
-    caplog.set_level(logging.INFO)
-    await connect_to_redis(redis_connection_string)
+    caplog.set_level(logging.WARNING)
+    await connect(redis_connection_string)
     for record in caplog.records:
         if record.levelname == "INFO":
-            assert "Found Redis 6." in record.message
+            assert "Found Redis 6" in record.message
