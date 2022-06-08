123a87cb0f6fb0f7bf50b85a978ff5fe742a3503
Aman Monga <aman260@outlook.com>
2022-06-08 09:38:20 -0700

    feat: consolidate redis code into core
    BREAKING CHANGE: Changes API for Redis utilities.

--- virtool_core/redis.py
+++ virtool_core/redis.py
@@ -1,9 +1,11 @@
 import asyncio
 import logging
 import sys
-from typing import Optional
 
-from aioredis import Redis, create_redis_pool
+from contextlib import asynccontextmanager, suppress
+from typing import Optional, AsyncGenerator
+
+from aioredis import Redis, create_redis_pool, Channel, ConnectionClosedError
 
 logger = logging.getLogger(__name__)
 
@@ -29,17 +31,14 @@ async def check_redis_server_version(redis: Redis) -> Optional[str]:
     return None
 
 
-async def connect_to_redis(
-        redis_connection_string: str,
-        ignore_error: bool = False
-) -> Redis:
+async def connect(redis_connection_string: str, timeout: int = 1) -> Redis:
     """
-    Create a connection to the Redis server specified in the passed `redis_connection_string`.
+    Create a connection to Redis server specified in passed `redis_connection_string`.
 
     Will exit the application if the server cannot be reached.
 
     :param redis_connection_string: the Redis connection string
-    :param ignore_error: if True, application will not exit if the connection fails
+    :param timeout: max time to open a connection
     :return: a Redis connection object
 
     """
@@ -47,19 +46,15 @@ async def connect_to_redis(
         logger.fatal("Invalid Redis connection string")
         sys.exit(1)
 
+    logger.info("Connecting to Redis")
+
     try:
-        redis = await create_redis_pool(redis_connection_string)
+        redis = await create_redis_pool(redis_connection_string, timeout=timeout)
         await check_redis_server_version(redis)
 
         return redis
     except ConnectionRefusedError:
-        message = "Could not connect to Redis: Connection refused"
-
-        if ignore_error:
-            logger.warning(message)
-            return None
-
-        logger.fatal(message)
+        logger.fatal("Could not connect to Redis: Connection refused")
         sys.exit(1)
 
 
@@ -67,7 +62,7 @@ async def periodically_ping_redis(redis: Redis):
     """
     Ping the Redis server every two minutes.
 
-    When using Azure Cache for Redis, connections that are inactive for more than 10 minutes
+    When using Azure Cache for Redis, connections inactive for more than 10 minutes
     are dropped. Regular pings prevent this from happening.
 
     :param redis: the Redis client
@@ -75,3 +70,45 @@ async def periodically_ping_redis(redis: Redis):
     while True:
         await asyncio.sleep(120)
         await redis.ping()
+
+
+async def resubscribe(redis: Redis, redis_channel_name: str) -> Channel:
+    """
+    Subscribe to the passed channel of the passed :class:`Redis` object.
+
+    :param redis: the Redis connection
+    :param redis_channel_name: name of the channel to reconnect to
+    :return: Channel
+
+    """
+    while True:
+        try:
+            (channel,) = await redis.subscribe(redis_channel_name)
+            return channel
+        except (ConnectionRefusedError, ConnectionResetError, ConnectionClosedError):
+            await asyncio.sleep(5)
+
+
+@asynccontextmanager
+async def configure_redis(
+        redis_connection_string: str,
+        timeout: int = 1
+) -> AsyncGenerator[Redis, None]:
+    """Prepare a redis connection."""
+    redis = None
+    ping_task = None
+
+    try:
+        redis = await connect(redis_connection_string, timeout)
+        ping_task = asyncio.create_task(periodically_ping_redis(redis))
+        yield redis
+    finally:
+        if ping_task is not None and redis is not None:
+            logger.info("Disconnecting from Redis")
+            ping_task.cancel()
+
+            with suppress(asyncio.CancelledError):
+                await ping_task
+
+            redis.close()
+            await redis.wait_closed()
