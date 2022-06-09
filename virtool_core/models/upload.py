a8b52d7b490ff19c5426bd74069ad3353bb29433
Ian Boyes <igboyes@gmail.com>
2022-06-09 14:11:48 -0700

    feat: add upload models

---
+++ virtool_core/models/upload.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel
+
+from virtool_core.models import UserMinimal
+
+
+class UploadMinimal(BaseModel):
+    """
+    Model for user uploads.
+    """
+
+    id: int
+    created_at: datetime
+    name: str
+    name_on_disk: str
+    ready: bool
+    removed: bool
+    removed_at: Optional[datetime]
+    reserved: bool
+    size: int
+    type: str
+    uploaded_at: datetime
+    user: UserMinimal
+
+
+Upload = UploadMinimal
