b8b2e1d281020c344bfd5db3f4ceb25089f0517e
Aman Monga <aman260@outlook.com>
2022-06-15 15:33:39 -0700

    feat: add history data models

---
+++ virtool_core/models/history.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from typing import Dict, Any
+
+from virtool_core.models.enums import HistoryMethod
+from virtool_core.models.reference import ReferenceMinimal
+from virtool_core.models.user import UserMinimal
+from pydantic import BaseModel
+
+
+class HistoryIndex(BaseModel):
+    id: str
+    version: int
+
+
+class HistoryOTU(BaseModel):
+    id: str
+    name: str
+    version: str
+
+
+class HistoryMinimal(BaseModel):
+    created_at: datetime
+    description: str
+    id: str
+    index: HistoryIndex
+    method_name: HistoryMethod
+    otu: HistoryOTU
+    reference: ReferenceMinimal
+    user: UserMinimal
+
+
+class History(HistoryMinimal):
+    diff: Dict[str, Any]
