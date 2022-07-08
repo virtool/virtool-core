3e479387c9a722f54d22648de04f21c74fd8d34a
Aman <aman260@outlook.com>
2022-07-08 13:33:51 -0700

    fix: add enum for quick_analyze_workflow

--- virtool_core/models/account.py
+++ virtool_core/models/account.py
@@ -1,9 +1,10 @@
+from virtool_core.models.enums import QuickAnalyzeWorkflow
 from virtool_core.models.user import User
 from pydantic import BaseModel
 
 
 class AccountSettings(BaseModel):
-    quick_analyze_workflow: str
+    quick_analyze_workflow: QuickAnalyzeWorkflow
     show_ids: bool
     show_versions: bool
     skip_quick_analyze_dialog: bool
