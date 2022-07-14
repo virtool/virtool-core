d66de3c2d2f8520be4364f38ed39d967f2543c25
Aman Monga <53246811+buddy326@users.noreply.github.com>
2022-07-13 17:16:29 -0700

    feat: add settings data model

---
+++ virtool_core/models/settings.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+
+class Settings(BaseModel):
+    sample_group: str = None
+    sample_group_read: bool = True
+    sample_group_write: bool = False
+    sample_all_read: bool = True
+    sample_all_write: bool = False
+    sample_unique_names: bool = True
+    hmm_slug: str = "virtool/virtool-hmm"
+    enable_api: bool = False
+    enable_sentry: bool = True
+    minimum_password_length: int = 8
+    default_source_types: list = ["isolate", "strain"]
