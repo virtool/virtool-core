b574414e2ef40c180f83981fab873c60f58564a2
Aman Monga <53246811+buddy326@users.noreply.github.com>
2022-08-16 14:14:54 -0700

    fix: use nested subtraction in analyses model

--- virtool_core/models/analysis.py
+++ virtool_core/models/analysis.py
@@ -6,7 +6,7 @@ from virtool_core.models.index import IndexNested
 from virtool_core.models.job import JobNested
 from virtool_core.models.reference import AnalysisReference
 from virtool_core.models.searchresult import SearchResult
-from virtool_core.models.subtraction import SubtractionMinimal
+from virtool_core.models.subtraction import SubtractionNested
 from virtool_core.models.user import UserMinimal
 
 
@@ -22,7 +22,7 @@ class AnalysisMinimal(BaseModel):
     ready: bool
     reference: AnalysisReference
     sample: AnalysisSample
-    subtractions: List[SubtractionMinimal]
+    subtractions: List[SubtractionNested]
     updated_at: datetime
     user: UserMinimal
     workflow: str
