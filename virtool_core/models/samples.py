6dc5ce62c8fecd35cfca8698306f33d250095bbb
Ian Boyes <igboyes@gmail.com>
2022-08-17 08:43:20 -0700

    fix: use correct type for sample read upload

--- virtool_core/models/samples.py
+++ virtool_core/models/samples.py
@@ -1,12 +1,13 @@
 from datetime import datetime
 from typing import List, Union, Optional
 
+from virtool_core.models.basemodel import BaseModel
 from virtool_core.models.enums import LibraryType
 from virtool_core.models.label import LabelNested
 from virtool_core.models.searchresult import SearchResult
 from virtool_core.models.subtraction import SubtractionNested
+from virtool_core.models.upload import Upload
 from virtool_core.models.user import UserMinimal
-from virtool_core.models.basemodel import BaseModel
 
 
 class SampleID(BaseModel):
@@ -44,7 +45,7 @@ class Read(BaseModel):
     name_on_disk: str
     sample: str
     size: int
-    upload: None
+    upload: Optional[Upload]
     uploaded_at: datetime
 
 
