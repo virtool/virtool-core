c43e05dfafa70331dc7d5776dd465305ae38e85f
Ian Boyes <igboyes@gmail.com>
2022-08-23 21:00:52 -0700

    style: remove unused import

--- virtool_core/models/job.py
+++ virtool_core/models/job.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 from typing import List, Dict, Any, Optional
 
-from virtool_core.models.user import UserMinimal, UserNested
 from virtool_core.models.basemodel import BaseModel
+from virtool_core.models.user import UserNested
 
 
 class JobError(BaseModel):
