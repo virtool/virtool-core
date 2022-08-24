50e20368afe73574ef80c8b4ed4e9f7cf341db9b
Ryan Fang <104466198+ryanfang5@users.noreply.github.com>
2022-08-24 13:42:35 -0700

    feat: add acquired job model and counts field to search result

--- virtool_core/models/job.py
+++ virtool_core/models/job.py
@@ -43,5 +43,10 @@ class Job(JobMinimal):
     status: List[JobStatus]
 
 
+class JobAcquired(Job):
+    key: str
+
+
 class JobSearchResult(SearchResult):
+    counts: Dict
     documents: List[JobMinimal]
