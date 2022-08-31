94b355e745929502dccebf54a31127ca19014f7a
Aman monga <aman260@outlook.com>
2022-08-31 13:48:20 -0700

    fix: fix email field and validator in account

--- virtool_core/models/account.py
+++ virtool_core/models/account.py
@@ -1,17 +1,20 @@
 from typing import Optional
 
 from email_validator import validate_email, EmailSyntaxError
-from pydantic import constr, validator, Field
+from pydantic import constr, validator
 
 from virtool_core.models.basemodel import BaseModel
 from virtool_core.models.enums import QuickAnalyzeWorkflow
 from virtool_core.models.user import User
 
 
-def check_email(email: Optional[str]) -> str:
+def check_email(email: Optional[str]) -> Optional[str]:
     """
     Checks if the given email is valid.
     """
+    if email is None:
+        return None
+
     try:
         validate_email(email)
     except EmailSyntaxError:
@@ -29,6 +32,6 @@ class AccountSettings(BaseModel):
 
 class Account(User):
     settings: AccountSettings
-    email: Optional[constr(strip_whitespace=True)] = Field(default="")
+    email: Optional[constr(strip_whitespace=True)] = None
 
     _email_validation = validator("email", allow_reuse=True)(check_email)
