44560544b4878e9d265621b0794f8db74b9d1568
reecehoffmann <reecehoffmann@outlook.com>
2022-10-21 15:50:29 -0700

    feat: add session model

---
+++ virtool_core/models/session.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from typing import List
+
+from virtool_core.models.basemodel import BaseModel
+from virtool_core.models.enums import Permission
+from virtool_core.models.group import Group
+from virtool_core.models.user import UserNested
+
+
+class Session(BaseModel):
+    created_at: datetime
+    ip: str
+    token: str
+    groups: List[Group]
+    permissions: Permission
+    force_reset: bool
+    user: UserNested
+
+
+
