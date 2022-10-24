4b39d792a286566fb3cd64d16ba58e8f3634c9b9
reecehoffmann <reecehoffmann@outlook.com>
2022-10-24 10:33:19 -0700

    fix: update session model to use single interface

--- virtool_core/models/session.py
+++ virtool_core/models/session.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from typing import List
+from typing import List, Optional
 
 from virtool_core.models.basemodel import BaseModel
 from virtool_core.models.enums import Permission
@@ -8,14 +8,22 @@ from virtool_core.models.group import Group
 from virtool_core.models.user import UserNested
 
 
-class MinimalSession(BaseModel):
-    created_at: datetime
-    ip: str
-
-
-class Session(MinimalSession):
+class SessionAuthentication(BaseModel):
     token: str
     groups: List[Group]
     permissions: Permission
     force_reset: bool
     user: UserNested
+
+
+class SessionReset(BaseModel):
+    code: str
+    remember: bool
+    user_id: str
+
+
+class Session(BaseModel):
+    authentication: Optional[SessionAuthentication]
+    reset: Optional[SessionReset]
+    created_at: datetime
+    ip: str
