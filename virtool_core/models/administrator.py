bf1d3b8f437a8c65aeef541f3421c767872e8181
Ryan Fang <104466198+ryanfang5@users.noreply.github.com>
2023-03-14 07:32:35 -0700

    feat: update administrator models
    * Update administrator models.
    * Add function to document enum values.

--- virtool_core/models/administrator.py
+++ virtool_core/models/administrator.py
@@ -1,6 +1,18 @@
+from typing import List
+
+from virtool_core.models.basemodel import BaseModel
 from virtool_core.models.user import UserNested
 from virtool_core.models.roles import AdministratorRole
 
 
-class Administrator(UserNested):
+class AdministratorMinimal(UserNested):
     role: AdministratorRole
+
+
+class Administrator(AdministratorMinimal):
+    available_roles: List[dict]
+
+
+class AdministratorSearch(BaseModel):
+    documents: List[AdministratorMinimal]
+    available_roles: List[dict]
