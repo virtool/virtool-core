3ca7352d674c89f364736ca2d9b6c28fea6ca6c1
Ian Boyes <igboyes@gmail.com>
2023-06-05 14:09:43 -0700

    fix: really fix missing import

--- virtool_core/models/samples.py
+++ virtool_core/models/samples.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
 from datetime import datetime
+from enum import Enum
 from typing import List, Union, TYPE_CHECKING, Optional
 
-from enums import Enum
+
 from virtool_core.models.basemodel import BaseModel
 from virtool_core.models.enums import LibraryType
 from virtool_core.models.job import JobMinimal
