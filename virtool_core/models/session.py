3c690dab69767f6a105d5ed95f4e915d7826e6e7
Reece Hoffmann <59776400+ReeceHoffmann@users.noreply.github.com>
2023-07-27 13:51:53 -0700

    fix: add `id` and remove secret fields from session model

--- virtool_core/models/session.py
+++ virtool_core/models/session.py
@@ -5,18 +5,17 @@ from virtool_core.models.basemodel import BaseModel
 
 
 class SessionAuthentication(BaseModel):
-    token: str
     user_id: str
 
 
 class SessionPasswordReset(BaseModel):
-    code: str
     remember: bool
     user_id: str
 
 
 class Session(BaseModel):
     authentication: Optional[SessionAuthentication]
-    reset: Optional[SessionPasswordReset]
     created_at: datetime
+    id: str
     ip: str
+    reset: Optional[SessionPasswordReset]
