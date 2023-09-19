45e170c817d99d97fcc528c0669b8d078863a185
Matthew <matthewcurtis1999@gmail.com>
2023-09-19 13:43:24 -0700

    test: update test to use new logging

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -2,18 +2,19 @@ import pytest
 
 from virtool_core.logging import configure_logs
 from structlog import get_logger
+from structlog.testing import capture_logs
 
 
 @pytest.mark.parametrize("debug,expected", [(False, 4), (True, 5)])
-def test_configure_logs(debug, expected, caplog):
-    logger = get_logger("ryanf")
-
+def test_configure_logs(debug, expected):
     configure_logs(debug)
+    with capture_logs() as cap:
+        logger = get_logger("ryanf")
 
-    logger.error("print error")
-    logger.warning("print warning")
-    logger.critical("print critical")
-    logger.info("print info")
-    logger.debug("print debug")
+        logger.error("print error")
+        logger.warning("print warning")
+        logger.critical("print critical")
+        logger.info("print info")
+        logger.debug("print debug")
 
-    assert len(caplog.records) == expected
+        assert len(cap) == expected
