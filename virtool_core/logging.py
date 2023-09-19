f937b5cb27ba1fcb2e1647c2006bfd3b7b081180
Matthew <matthewcurtis1999@gmail.com>
2023-09-19 13:24:36 -0700

    feat!: implement structlog for logging

--- virtool_core/logging.py
+++ virtool_core/logging.py
@@ -1,24 +1,30 @@
-from logging import INFO, DEBUG, captureWarnings
-
-import coloredlogs
+import logging
+from logging import INFO, DEBUG
+import structlog
 
 
 def configure_logs(debug: bool):
     """
     Configure logging for Virtool.
 
-    * Use colored logging.
-    * Set short or long line formatting based on configuration options.
+    * Use structlog.
     * Set logging level based on ``dev`` configuration option.
     :param debug: log debug messages
 
     """
 
-    log_format = "{asctime:<20} {module:<11} {levelname:<8} {message}"
+    logging.basicConfig(level=DEBUG if debug else INFO, format="%(message)s")
 
-    if debug:
-        log_format += " ({name}:{funcName}:{lineno})"
-
-    captureWarnings(True)
-
-    coloredlogs.install(level=DEBUG if debug else INFO, fmt=log_format, style="{")
+    structlog.configure(
+        wrapper_class=structlog.make_filtering_bound_logger(DEBUG if debug else INFO),
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        cache_logger_on_first_use=True,
+        processors=[
+            structlog.stdlib.filter_by_level,
+            structlog.stdlib.add_logger_name,
+            structlog.stdlib.add_log_level,
+            structlog.stdlib.PositionalArgumentsFormatter(),
+            structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M.%S"),
+            structlog.dev.ConsoleRenderer(colors=True, sort_keys=True),
+        ],
+    )
