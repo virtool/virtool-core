21aaf23fa2965128576e679943e2f11bb4ec18fd
Matthew <matthewcurtis1999@gmail.com>
2024-04-09 10:52:21 -0700

    chore: change wrapper to not inherit

--- virtool_core/redis.py
+++ virtool_core/redis.py
@@ -1,8 +1,10 @@
 import asyncio
+
+from redis.commands.core import ResponseT
 from structlog import get_logger
 import sys
 from contextlib import asynccontextmanager, suppress
-from typing import Optional, AsyncGenerator
+from typing import Optional, AsyncGenerator, Union, Awaitable
 import redis.asyncio
 import redis.exceptions
 
@@ -10,24 +12,69 @@ logger = get_logger(__name__)
 
 
 class Redis(redis.asyncio.Redis):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, redis_connection_string, **kwargs):
+        self._client = redis.asyncio.from_url(redis_connection_string, **kwargs)
+
+    async def client(self, *args, **kwargs):
+        return self._client.client()
+
+    async def get(self, *args, **kwargs):
+        return await self._client.get(*args, **kwargs)
+
+    async def set(self, key, value, ex=None, nx=False, **kwargs):
+        return await self._client.set(key, value, ex=ex, nx=nx, **kwargs)
+
+    async def delete(self, key):
+        return await self._client.delete(key)
+
+    async def ttl(self, session_identifier):
+        return await self._client.ttl(session_identifier)
 
     async def subscribe(self, channel_name: str):
-        channel = self.pubsub()
+        channel = self._client.pubsub()
         await channel.subscribe(channel_name)
-        return (channel,)
+        return channel
+
+    async def publish(self, channel_name: str, message: str):
+        return await self._client.publish(channel_name, message)
+
+    async def llen(self, name: str) -> Union[Awaitable[int], int]:
+        return await self._client.llen(name)
 
     async def lrange(self, name, start: int, end: int):
-        names_list = await super().lrange(name, start, end)
+        names_list = await self._client.lrange(name, start, end)
         return names_list
+
     async def lpop(self, key):
-        return (await super().lpop(key)).decode('utf-8')
+        return (await self._client.lpop(key)).decode('utf-8')
 
+    async def lrem(self, key, count, element):
+        return await self._client.lrem(key, count, element)
 
-class Channel(redis.asyncio.client.PubSub):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    async def rpush(self, key, *values):
+        return await self._client.rpush(key, *values)
+
+    async def flushdb(self, asynchronous: bool = False, **kwargs) -> ResponseT:
+        return await self._client.flushdb(asynchronous)
+
+    async def close(self):
+        return await self._client.close()
+
+    async def check_redis_server_version(self) -> Optional[str]:
+        """
+        Check the version of the server represented in the passed :class:`Redis` object.
+
+        The version is logged and returned by the function.
+
+        :param redis: the Redis connection
+        :return: the version
+        """
+        info = await self._client.info()
+        version = info["redis_version"]
+        logger.info(f"Found Redis {version}")
+
+        return version
+
 
 
 class ConnectionClosedError(redis.exceptions.TimeoutError):
@@ -38,26 +85,6 @@ class ConnectionClosedError(redis.exceptions.TimeoutError):
 class ChannelClosedError(redis.exceptions.TimeoutError):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-
-def create_redis_pool(redis_connection_string: str, **kwargs) -> Redis:
-    return Redis.from_url(redis_connection_string, **kwargs)
-
-
-async def check_redis_server_version(redis: Redis) -> Optional[str]:
-    """
-    Check the version of the server represented in the passed :class:`Redis` object.
-
-    The version is logged and returned by the function.
-
-    :param redis: the Redis connection
-    :return: the version
-    """
-    info = await redis.info()
-    version = info["redis_version"]
-    logger.info(f"Found Redis {version}")
-
-    return version
 
 
 async def connect(redis_connection_string: str) -> Redis:
@@ -78,8 +105,8 @@ async def connect(redis_connection_string: str) -> Redis:
     logger.info("Connecting to Redis")
 
     try:
-        redis = create_redis_pool(redis_connection_string)
-        await check_redis_server_version(redis)
+        redis = Redis(redis_connection_string)
+        await redis.check_redis_server_version()
 
         return redis
     except ConnectionRefusedError:
@@ -101,7 +128,7 @@ async def periodically_ping_redis(redis: Redis):
         await redis.ping()
 
 
-async def resubscribe(redis: Redis, redis_channel_name: str) -> Channel:
+async def resubscribe(redis: Redis, redis_channel_name: str):
     """
     Subscribe to the passed channel of the passed :class:`Redis` object.
 
