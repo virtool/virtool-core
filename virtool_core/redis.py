afdd3f22722215eb282aacaf583cb241202064c6
Ian <igboyes@gmail.com>
2024-04-29 16:18:20 -0700

    feat: clean up redis wrapper

--- virtool_core/redis.py
+++ virtool_core/redis.py
@@ -1,174 +1,279 @@
 import asyncio
-from redis.commands.core import ResponseT
-from structlog import get_logger
-import sys
-from contextlib import suppress
-from typing import Optional, Union, Awaitable
+import datetime
+import json
+from typing import AsyncGenerator, TypeAlias
+
+import arrow
 import redis.asyncio
 import redis.exceptions
+from orjson import orjson
+from redis import ConnectionError
+from structlog import get_logger
 
-logger = get_logger(__name__)
+logger = get_logger("redis")
+
+RedisElement: TypeAlias = float | int | str | dict
 
 
-class ChannelClosedError(Exception):
-    pass
+class RedisError(Exception):
+    """A generic error raised when interacting with Redis."""
 
 
-class Redis(redis.asyncio.Redis):
-    def __init__(self, redis_connection_string, **kwargs):
-        self._client = redis.asyncio.from_url(redis_connection_string, **kwargs)
-        self.redis_connection_string = redis_connection_string
-        self._ping_task = None
+class RedisChannelClosedError(RedisError):
+    """An error raised when a Redis channel is closed while it is being read."""
+
+
+def _coerce_redis_request(value: RedisElement | None) -> bytes | int | float:
+    if isinstance(value, (int, float)):
+        return value
+
+    if isinstance(value, dict):
+        return orjson.dumps(value)
+
+    return str(value).encode("utf-8")
+
+
+def _coerce_redis_response(value: bytes | str | int | None) -> RedisElement | None:
+    if value is None:
+        return None
+
+    decoded = value.decode("utf-8")
+
+    try:
+        return int(decoded)
+    except ValueError:
+        ...
+
+    try:
+        return json.loads(decoded)
+    except json.JSONDecodeError:
+        ...
+
+    return decoded
+
+
+class Redis:
+    """A Redis client based on ``redis``.
+
+    Example:
+    -------
+    .. code-block:: python
+
+        async with Redis("redis://localhost:6379") as redis:
+            await redis.set("virtool", "The best virus detection platform")
+
+            value = await redis.get("virtool"))
+            # The best virus detection platform
+
+    Features:
+
+    * After successful connection, the client automatically pings the server to keep the
+      connection alive.
+    * The connection is automatically closed when the context manager exits.
+    * Dictionaries, strings, integers, and floats are automatically serialized and
+      deserialized.
+
+    :param connection_string: the connection string for the Redis server
+
+    """
+
+    def __init__(self, connection_string: str):
+        if not connection_string.startswith("redis://"):
+            raise RedisError("Invalid Redis connection string")
+
+        self._client = redis.asyncio.from_url(connection_string)
+        """The underlying Redis client object."""
+
+        self._client_info: dict | None = None
+        """The Redis server information, first fetched when the connection is opened."""
+
+        self._ping_task: asyncio.Task | None = None
+        """A task that pings the Redis server every two minutes to keep the connection
+        alive.
+        """
+
+        self.last_pong: datetime.datetime | None = None
+        """The time of the last successful ping.
+        
+        `None` if no pings have been acknowledged.        
+        """
 
     async def __aenter__(self):
-        self._client = await connect(self.redis_connection_string)
-        self._ping_task = asyncio.create_task(self.periodically_ping())
+        logger.info("Connecting to Redis")
+
+        try:
+            self._client_info = await self._client.info()
+        except ConnectionError as e:
+            if "Connect call failed" in str(e):
+                raise RedisError("Could not connect")
+
+            raise RedisError(f"Unhandled client error: {e}")
+
+        self._ping_task = asyncio.create_task(self._ping())
+
         return self
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
-        if self._ping_task is not None:
+        if self._ping_task:
             logger.info("Disconnecting from Redis")
             self._ping_task.cancel()
+            await self._ping_task
 
-            with suppress(asyncio.CancelledError):
-                await self._ping_task
+        if self._client:
+            await self._client.aclose()
 
-        if self._client is not None:
-            await self._client.close()
-
-    async def get(self, *args, **kwargs):
-        return await self._client.get(*args, **kwargs)
-
-    async def set(self, key, value, ex=None, nx=False, **kwargs):
-        return await self._client.set(key, value, ex=ex, nx=nx, **kwargs)
-
-    async def delete(self, key):
-        return await self._client.delete(key)
-
-    async def ttl(self, session_identifier):
-        return await self._client.ttl(session_identifier)
-
-    async def subscribe(self, channel_name: str):
-        channel = Channel(self._client)
-        await channel.subscribe(channel_name)
-        return channel
-
-    async def publish(self, channel_name: str, message: str):
-        return await self._client.publish(channel_name, message)
-
-    async def blpop(self, *args):
-        return await self._client.blpop(*args)
-
-    async def llen(self, name: str) -> Union[Awaitable[int], int]:
-        return await self._client.llen(name)
-
-    async def lrange(self, name, start: int, end: int):
-        names_list = await self._client.lrange(name, start, end)
-        return names_list
-
-    async def lpop(self, key):
-        return (await self._client.lpop(key)).decode('utf-8')
-
-    async def lrem(self, key, count, element):
-        return await self._client.lrem(key, count, element)
-
-    async def rpush(self, key, *values):
-        return await self._client.rpush(key, *values)
-
-    async def flushdb(self, asynchronous: bool = False, **kwargs) -> ResponseT:
-        return await self._client.flushdb(asynchronous)
-
-    async def close(self):
-        return await self._client.close()
-
-    async def check_redis_server_version(self) -> Optional[str]:
-        """
-        Check the version of the server represented in the passed :class:`Redis` object.
-
-        The version is logged and returned by the function.
-
-        :param redis: the Redis connection
-        :return: the version
-        """
-        info = await self._client.info()
-        version = info["redis_version"]
-        logger.info(f"Found Redis {version}")
-
-        return version
-
-    async def periodically_ping(self):
-        """
-        Ping the Redis server every two minutes.
+    async def _ping(self):
+        """Ping the Redis server every two minutes.
 
         When using Azure Cache for Redis, connections inactive for more than 10 minutes
         are dropped. Regular pings prevent this from happening.
 
         """
-        while True:
-            await asyncio.sleep(120)
-            await self._client.ping()
-
-
-class Channel(redis.client.PubSub):
-    def __init__(self, client):
-        self._channel = client.pubsub()
-
-    def subscribe(self, channel_name: str):
-        return self._channel.subscribe(channel_name)
-
-    def listen(self):
-        return self._channel.listen()
-
-    def get_message(
-            self, ignore_subscribe_messages: bool = False, timeout: float = 0.0
-    ):
         try:
-            return self._channel.get_message(ignore_subscribe_messages, timeout)
-        except redis.exceptions.TimeoutError:
-            raise ChannelClosedError
+            while True:
+                if await self._client.ping() is True:
+                    self.last_pong = arrow.utcnow().naive
 
-    def unsubscribe(self, *args):
-        return self._channel.unsubscribe(*args)
+                await asyncio.sleep(120)
 
+        except asyncio.CancelledError:
+            ...
 
-async def connect(redis_connection_string: str) -> Redis:
-    """
-    Create a connection to Redis server specified in passed `redis_connection_string`.
-
-    Will exit the application if the server cannot be reached.
-
-    :param redis_connection_string: the Redis connection string
-    :return: a Redis connection object
-
-    """
-    if not redis_connection_string.startswith("redis://"):
-        logger.fatal("Invalid Redis connection string")
-        sys.exit(1)
-
-    logger.info("Connecting to Redis")
-
-    try:
-        redis_con = Redis(redis_connection_string)
-        await redis_con.check_redis_server_version()
-
-        return redis_con
-    except ConnectionRefusedError:
-        logger.fatal("Could not connect to Redis: Connection refused")
-        sys.exit(1)
-
-
-async def resubscribe(redis_con: Redis, redis_channel_name: str):
-    """
-    Subscribe to the passed channel of the passed :class:`Redis` object.
-
-    :param redis_con: the Redis connection
-    :param redis_channel_name: name of the channel to reconnect to
-    :return: Channel
-
-    """
-    while True:
+    @property
+    def server_version(self) -> str:
+        """The version of the connected Redis server."""
         try:
-            channel = await redis_con.pubsub().subscribe(redis_channel_name)
-            return channel
-        except ConnectionError:
-            await asyncio.sleep(5)
+            return self._client_info["redis_version"]
+        except TypeError:
+            raise RedisError(
+                "Could not get server version because server info is not available.",
+            )
+
+    async def get(self, key: str) -> RedisElement | None:
+        """Get the value at ``key``.
+
+        :param key: the key to get
+        :return: the value at ``key``
+        """
+        return _coerce_redis_response(await self._client.get(key))
+
+    async def set(self, key: str, value: RedisElement, expire: int = 0):
+        """Set the value at ``key`` to value with an optional expiration time in
+        seconds.
+
+        :param key: the key to set
+        :param value: the value to set
+        :param expire: the expiration time in seconds
+
+        """
+        if expire == 0:
+            await self._client.set(key, _coerce_redis_request(value))
+        else:
+            await self._client.setex(key, expire, _coerce_redis_request(value))
+
+    async def delete(self, key: str):
+        """Delete the value at ``key``."""
+        await self._client.delete(key)
+
+    async def ttl(self, key: str) -> int:
+        """Get the time-to-live for a ``key`` in seconds."""
+        return await self._client.ttl(key)
+
+    async def subscribe(self, channel_name: str) -> AsyncGenerator[RedisElement, None]:
+        """Subscribe to a channel with ``channel_name`` and yield messages.
+
+        Example:
+        -------
+        .. code-block:: python
+
+                async for message in redis.subscribe("channel:cancel"):
+                    print(message)
+
+        :param channel_name: the name of the channel to subscribe to
+        :return: an async generator that yields messages
+
+        """
+        async with self._client.pubsub() as pubsub:
+            await pubsub.subscribe(channel_name)
+
+            while True:
+                message = await pubsub.get_message(ignore_subscribe_messages=True)
+
+                if message is None:
+                    continue
+
+                yield _coerce_redis_response(message["data"])
+
+    async def publish(self, channel_name: str, message: RedisElement):
+        """Publish a message to a channel.
+
+        :param channel_name: the name of the channel to publish to
+        :param message: the message to publish
+
+        """
+        await self._client.publish(channel_name, _coerce_redis_request(message))
+
+    async def blpop(self, key: str) -> RedisElement:
+        """Remove and return the first element of the list at ``key``, or block until
+        one is available.
+
+        """
+        _, value = await self._client.blpop([key])
+        return _coerce_redis_response(value)
+
+    async def llen(self, key: str) -> int:
+        """Get the length of the list at ``key``.
+
+        :param key: the key of the list to get the length of
+        :return: the length of the list
+        """
+        return await self._client.llen(key)
+
+    async def lrange(self, key: str, start: int, end: int) -> list[RedisElement]:
+        """Get a range of elements from the list at ``key``.
+
+        :param key: the key of the list to get from
+        :param start: the start index
+        :param end: the end index
+        :return: a list of elements
+
+        """
+        return [
+            _coerce_redis_response(value)
+            for value in await self._client.lrange(key, start, end)
+        ]
+
+    async def lpop(self, key: str):
+        """Remove and return the first element of the list at ``key``."""
+        return _coerce_redis_response(await self._client.lpop(key))
+
+    async def lrem(self, key: str, count: int, element: str):
+        """Remove the first ``count`` elements from the list at ``key``.
+
+        The ``count`` argument influences the following behaviours:
+
+        * count > 0: Remove head to tail.
+        * count < 0: Remove tail to head.
+        * count = 0: Remove everywhere.
+
+        :param key: the key of the list to remove from
+        :param count: the number of elements to remove
+        :param element: the element to remove
+
+        """
+        return await self._client.lrem(key, count, element)
+
+    async def rpush(self, key: str, *values):
+        """Push ``values`` onto the tail of the list ``key``.
+
+        :param key: the key of the list to push to
+        :param values: the values to push
+        """
+        return await self._client.rpush(
+            key,
+            *[_coerce_redis_request(v) for v in values],
+        )
+
+    async def flushdb(self):
+        """Delete all keys in the current database."""
+        await self._client.flushdb(asynchronous=False)
