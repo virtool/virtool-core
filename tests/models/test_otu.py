c87e08929619784f7c775692928211f2ab7c04df
Reece Hoffmann <59776400+ReeceHoffmann@users.noreply.github.com>
2024-06-11 15:08:47 -0700

    fix: allow `None` for` `OTUSequence.host`

--- tests/models/test_otu.py
+++ tests/models/test_otu.py
@@ -1,9 +1,22 @@
-from virtool_core.models.otu import OTUSegment
+from virtool_core.models.otu import OTUSegment, OTUSequence
 
 
 def test_molecule_nullable():
-    segment = OTUSegment(**{"molecule": "", "name": "RNA 1", "required": False})
+    segment = OTUSegment(molecule="", name="RNA 1", required=False)
     assert segment.molecule is None
 
-    segment = OTUSegment(**{"molecule": "ssRNA", "name": "RNA 1", "required": False})
+    segment = OTUSegment(molecule="ssRNA", name="RNA 1", required=False)
     assert segment.molecule == "ssRNA"
+
+
+def test_host_nullable():
+    sequence = {
+        "accession": "test_accession",
+        "definition": "test_definition",
+        "id": "test_id",
+        "sequence": "ATCD",
+    }
+
+    assert OTUSequence(**sequence).host == ""
+
+    assert OTUSequence(**{**sequence, "host": "test_host"}).host == "test_host"
